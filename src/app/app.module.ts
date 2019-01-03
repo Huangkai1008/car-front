2a338729f5e28dc887e05155324c51d31137743e
huangkai <18778335525@163.com>
2019-01-03 22:08:18 +0800

    获取信息并展示

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { CarListComponent } from './car-list/car-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/car/car.service.ts
+++ src/app/shared/car/car.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // 获取所有的cool-cars信息
+  getAll(): Observable<any> {
+    return this.http.get('//localhost:8080/cars');
+  }
 }
